f8ecd4aa65bede32f203959577afca442521630d
katrinTarneva <112895125+katrinTarneva@users.noreply.github.com>
2023-11-06 12:15:14 +0100

    portfolio version 1

---
+++ src/components/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+const Home = () => {
+    return (
+        <div name='home' className='w-full h-screen bg-[#497359]'>
+
+            {/*Container*/}
+            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+                <p className='text-[#fabfbc]'>Hi, my name is</p>
+                <h1 className='text-4xl sm:text-7xl font-bold text-[#34D973]'>Katrin Tarneva</h1>
+                <h2 className='text-4xl sm:text-7xl font-bold text-[#BF7C78]'>I am a Software Engineer.</h2>
+                <p className='text-[#fabfbc] py-4 max-w-[700px]'>I'am a Full Stack Software Engineer specializing in Game Development and XR
+                    (occasinally i am also designing, 3D modeling and level building).
+                    Currently my focus is XR Development (Virtual Reality and Augmented Reality)
+                </p>
+                <div>
+                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#5DAA7A] hover:border-[#5DAA7A]'>View Work
+                        <span className='group-hover:rotate-90 duration-300'>
+                            <HiArrowNarrowRight className='ml-3' />
+                        </span> </button>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
